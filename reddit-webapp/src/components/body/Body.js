4bbd6380bec202f67c7fdc93fc09463f5701f1c3
dselimovic <dselimovic1@etf.unsa.ba>
2020-08-27 23:48:40 +0200

    fixed body

--- reddit-webapp/src/components/body/Body.js
+++ reddit-webapp/src/components/body/Body.js
@@ -1,17 +1,8 @@
 import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
 import PostFilter from "./PostFilter";
 import PostList from './PostList';
-import { fetchPostVotes } from "../../actions";
-import { getUser } from "../../user";
 
 const Body = props => {
-
-    const { fetchPostVotes, votes } = props;
-
-    useEffect(() => {
-        if (getUser()) fetchPostVotes();
-    }, [fetchPostVotes, votes]);
 
     return (
         <div className="ui container" style={{backgroundColor: '#d1d7e8'}}>
@@ -23,8 +14,4 @@ const Body = props => {
 
 }
 
-const mapStateToProps = state => {
-    return { votes: state.votes };
-};
-
-export default connect(mapStateToProps, { fetchPostVotes })(Body);
+export default Body;
