43c5f9e516f0c92e21039c757dd60b63b3f76098
dselimovic <dselimovic1@etf.unsa.ba>
2020-08-27 20:24:25 +0200

    added reducer for user topics

--- reddit-webapp/src/reducers/index.js
+++ reddit-webapp/src/reducers/index.js
@@ -4,11 +4,13 @@ import authReducer from "./authReducer";
 import postReducer from "./postReducer";
 import postVoterReducer from "./postVoterReducer";
 import commentVoterReducer from "./commentVoterReducer";
+import userTopicsReducer from "./userTopicsReducer";
 
 export default combineReducers({
     auth: authReducer,
     posts: postReducer,
     votes: postVoterReducer,
     commentVotes: commentVoterReducer,
+    userTopics: userTopicsReducer,
     form: formReducer
 });
