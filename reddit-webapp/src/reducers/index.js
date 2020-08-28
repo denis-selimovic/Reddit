2b09876dac606905282f29cdc25d347a5f058c9c
dselimovic <dselimovic1@etf.unsa.ba>
2020-08-29 00:58:01 +0200

    added reducer for comments

--- reddit-webapp/src/reducers/index.js
+++ reddit-webapp/src/reducers/index.js
@@ -3,10 +3,12 @@ import { reducer as formReducer } from "redux-form";
 import authReducer from "./authReducer";
 import postReducer from "./postReducer";
 import userTopicsReducer from "./userTopicsReducer";
+import commentReducer from "./commentReducer";
 
 export default combineReducers({
     auth: authReducer,
     posts: postReducer,
     userTopics: userTopicsReducer,
+    comments: combineReducers,
     form: formReducer
 });
