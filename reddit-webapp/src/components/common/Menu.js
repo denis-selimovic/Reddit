d9e2eea5099a3b139d094c19fd46bd304e5ca637
dselimovic <dselimovic1@etf.unsa.ba>
2020-08-28 17:38:45 +0200

    implemented dropdown

--- reddit-webapp/src/components/common/Menu.js
+++ reddit-webapp/src/components/common/Menu.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Menu = ({ options }) => {
 
+    const [selected, setSelected] = useState('All');
+
+    const onOptionChosen = key => {
+        options[key].callback()
+        setSelected(options[key].value);
+    };
 
     const renderOptions = () => {
         return Object.keys(options).map(key => {
             return (
-                <div key={key} className="item" data-value={key} onClick={e => options[key].callback()}>{options[key].value}</div>
+                <div key={key} className="item" data-value={key} onClick={e => onOptionChosen(key)}>{options[key].value}</div>
             );
         });
     };
 
     return (
-        <div className="ui simple fluid selection dropdown" style={{zIndex: '10'}}>
+        <div className="ui simple selection dropdown" style={{zIndex: '10'}}>
             <input type="hidden" name="user" />
+            <div className="default text">{selected}</div>
             <i className="dropdown icon"/>
             <div className="menu">{renderOptions()}</div>
         </div>
