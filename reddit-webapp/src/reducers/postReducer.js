741227be377047010d99ac99810a3bddf31b3d8a
dselimovic <dselimovic1@etf.unsa.ba>
2020-08-28 22:37:50 +0200

    added action to reducer

--- reddit-webapp/src/reducers/postReducer.js
+++ reddit-webapp/src/reducers/postReducer.js
@@ -1,5 +1,13 @@
 import _ from 'lodash';
-import {CREATE_POST, DOWNVOTE, FETCH_POSTS, FETCH_SUBSCRIBED_POSTS, FETCH_USER_POSTS, UPVOTE} from "../actions/types";
+import {
+    CREATE_POST,
+    DELETE_POST,
+    DOWNVOTE,
+    FETCH_POSTS,
+    FETCH_SUBSCRIBED_POSTS,
+    FETCH_USER_POSTS,
+    UPVOTE
+} from "../actions/types";
 
 export default (state = {}, action) => {
     switch (action.type) {
@@ -15,6 +23,8 @@ export default (state = {}, action) => {
             return { ..._.mapKeys(action.payload, 'id') };
         case CREATE_POST:
             return { ...state, [action.payload.id]: action.payload };
+        case DELETE_POST:
+            return _.omit(state, action.payload);
         default:
             return state;
     }
