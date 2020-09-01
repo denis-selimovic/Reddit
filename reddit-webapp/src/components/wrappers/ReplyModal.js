6b146448809f915ee846484742e9d2d6a585f95b
dselimovic <dselimovic1@etf.unsa.ba>
2020-09-01 22:29:30 +0200

    successfull api call

--- reddit-webapp/src/components/wrappers/ReplyModal.js
+++ reddit-webapp/src/components/wrappers/ReplyModal.js
@@ -1,21 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { connect } from 'react-redux';
+import { replyComment } from "../../actions";
 import history from "../../history";
 import CreateComment from "../forms/CreateComment";
 
-const ReplyModal = ({ match, location }) => {
+const ReplyModal = ({ match, location, replyComment }) => {
 
     const { id } = match.params;
-    const postId = location.search.match(/\d+/);
+    const postId = location.search.match(/\d+/)[0];
 
     const cancel = () => history.push(`/posts/${postId}`)
-    const action = () => {}
+    const action = formValues => replyComment(id, { post: postId, text: formValues.text });
 
     return ReactDOM.createPortal(
         <div className="ui dimmer modals visible active" onClick={() => cancel()}>
             <div className="ui standard modal visible active" onClick={e => e.stopPropagation()} style={{paddingBottom: '50px'}}>
                 <div className="ui item" style={{marginTop: '50px', marginBottom: '50 px'}}>
-                    <CreateComment/>
+                    <CreateComment onSubmit={action}/>
                 </div>
             </div>
         </div>,
@@ -23,4 +25,4 @@ const ReplyModal = ({ match, location }) => {
     )
 };
 
-export default ReplyModal;
+export default connect(null, { replyComment })(ReplyModal);
