1fc6c352779fe70eaee14b0c8aaa90571b5f3aa8
dselimovic <dselimovic1@etf.unsa.ba>
2020-09-01 22:11:27 +0200

    added reducer for replying

--- reddit-webapp/src/reducers/commentReducer.js
+++ reddit-webapp/src/reducers/commentReducer.js
@@ -1,9 +1,13 @@
-import {CREATE_COMMENT} from "../actions/types";
+import {CREATE_COMMENT, REPLY_COMMENT} from "../actions/types";
 
 export default (state = {}, action) => {
     switch (action.type) {
         case CREATE_COMMENT:
             return { ...state, [action.payload.id]: action.payload };
+        case REPLY_COMMENT:
+            const { id, data } = action.payload;
+            state[id].children.concat(data);
+            return { ...state, [data.id]: data };
         default:
             return state;
     }
